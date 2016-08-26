2bdcb858418072bc0c8d41b9fa622107b83d7592
Risul <risul321@gmail.com>
2016-08-27 04:09:03 +0600

    - Fixed bug on post comment show

--- src/views/comment.blade.php
+++ src/views/comment.blade.php
@@ -70,3 +70,4 @@ foreach ($comments as $comment) {
     </div>
     <button class="ui basic button" id="showComment" data-show-comment="0" data-item-id="{{ $comment_item_id }}">Show comments</button>
 </div>
+<style>.comment{display:none;}</style>
