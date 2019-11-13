984a7a4f7c5e12debaffbaf14f29b16affee0440
Risul Islam <risul321@gmail.com>
2019-11-13 13:31:25 +0600

    Update travis & test

---
+++ tests/Test.php
@@ -0,0 +1,18 @@
+
+<?php
+namespace risul\LaravelLikeComment\Tests\Unit;
+
+use risul\LaravelLikeComment\Tests\PackageTestCase;
+
+class Test extends PackageTestCase
+{
+    /**
+     * A basic test example.
+     *
+     * @return void
+     */
+    public function testBasicTest()
+    {
+        $this->assertTrue(true);
+    }
+}
